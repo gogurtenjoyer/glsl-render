ec517a0aceb7261fd276a20454b6a70f7489d600
gogurtenjoyer <36354352+gogurtenjoyer@users.noreply.github.com>
2024-08-11 13:27:33 -0400

    fixed default vert.glsl

--- shaders/default/vert.glsl
+++ shaders/default/vert.glsl
@@ -1,10 +1,10 @@
 #version 330
 
 in vec2 in_vert;
-in vec2 in_text;
-out vec2 v_text;
+in vec2 in_uv;
+out vec2 uv;
 
 void main() {
     gl_Position = vec4(in_vert, 0.0, 1.0);
-    v_text = vec2(in_text.x, 1.0 - in_text.y);;
+    uv = in_uv;
 }
